6283754cc2da031c4f11590dc892967b6bd269be
Gigson Aquino <gigsonaquino4@gmail.com>
2025-11-30 10:18:14 +0800

    updated

--- CarRentalCataloguee/LogInForm.Designer.cs
+++ CarRentalCataloguee/LogInForm.Designer.cs
@@ -29,26 +29,39 @@
         private void InitializeComponent()
         {
             flowLayoutPanel1 = new FlowLayoutPanel();
+            pictureBox1 = new PictureBox();
             panel1 = new Panel();
             label2 = new Label();
             label1 = new Label();
-            button1 = new Button();
-            textBox2 = new TextBox();
-            textBox1 = new TextBox();
+            btnLogin = new Button();
+            txtPassword = new TextBox();
+            txtUserName = new TextBox();
             label3 = new Label();
-            button2 = new Button();
+            btnSignUp = new Button();
             label4 = new Label();
+            flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             flowLayoutPanel1.BackColor = Color.SteelBlue;
+            flowLayoutPanel1.Controls.Add(pictureBox1);
             flowLayoutPanel1.Dock = DockStyle.Left;
             flowLayoutPanel1.Location = new Point(0, 0);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
             flowLayoutPanel1.Size = new Size(311, 450);
             flowLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Bottom;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(308, 229);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
             // panel1
             // 
@@ -56,9 +69,9 @@
             panel1.BorderStyle = BorderStyle.Fixed3D;
             panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
-            panel1.Controls.Add(button1);
-            panel1.Controls.Add(textBox2);
-            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(btnLogin);
+            panel1.Controls.Add(txtPassword);
+            panel1.Controls.Add(txtUserName);
             panel1.Location = new Point(376, 99);
             panel1.Name = "panel1";
             panel1.Size = new Size(363, 260);
@@ -69,68 +82,68 @@
             label2.AutoSize = true;
             label2.Location = new Point(32, 100);
             label2.Name = "label2";
-            label2.Size = new Size(38, 15);
+            label2.Size = new Size(57, 15);
             label2.TabIndex = 4;
-            label2.Text = "label2";
+            label2.Text = "Password";
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Location = new Point(32, 44);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(62, 15);
             label1.TabIndex = 3;
-            label1.Text = "label1";
+            label1.Text = "UserName";
             // 
-            // button1
+            // btnLogin
             // 
-            button1.Location = new Point(150, 217);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 2;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnLogin.Location = new Point(150, 217);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 23);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "Log-in";
+            btnLogin.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // txtPassword
             // 
-            textBox2.Location = new Point(32, 118);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(296, 23);
-            textBox2.TabIndex = 1;
+            txtPassword.Location = new Point(32, 118);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(296, 23);
+            txtPassword.TabIndex = 1;
             // 
-            // textBox1
+            // txtUserName
             // 
-            textBox1.Location = new Point(32, 62);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(296, 23);
-            textBox1.TabIndex = 0;
+            txtUserName.Location = new Point(32, 62);
+            txtUserName.Name = "txtUserName";
+            txtUserName.Size = new Size(296, 23);
+            txtUserName.TabIndex = 0;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(433, 426);
+            label3.Location = new Point(444, 422);
             label3.Name = "label3";
-            label3.Size = new Size(38, 15);
+            label3.Size = new Size(137, 15);
             label3.TabIndex = 2;
-            label3.Text = "label3";
+            label3.Text = "You dont have account? ";
             // 
-            // button2
+            // btnSignUp
             // 
-            button2.Location = new Point(606, 418);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 5;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            btnSignUp.Location = new Point(587, 418);
+            btnSignUp.Name = "btnSignUp";
+            btnSignUp.Size = new Size(75, 23);
+            btnSignUp.TabIndex = 5;
+            btnSignUp.Text = "Sign-up";
+            btnSignUp.UseVisualStyleBackColor = true;
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Location = new Point(526, 49);
             label4.Name = "label4";
-            label4.Size = new Size(38, 15);
+            label4.Size = new Size(88, 15);
             label4.TabIndex = 6;
-            label4.Text = "label4";
+            label4.Text = "Welcome Back!";
             // 
             // LogInForm
             // 
@@ -138,12 +151,14 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Controls.Add(label4);
-            Controls.Add(button2);
+            Controls.Add(btnSignUp);
             Controls.Add(label3);
             Controls.Add(panel1);
             Controls.Add(flowLayoutPanel1);
             Name = "LogInForm";
             Text = "Form1";
+            flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ResumeLayout(false);
@@ -154,13 +169,14 @@
 
         private FlowLayoutPanel flowLayoutPanel1;
         private Panel panel1;
-        private TextBox textBox1;
+        private TextBox txtUserName;
         private Label label2;
         private Label label1;
-        private Button button1;
-        private TextBox textBox2;
+        private Button btnLogin;
+        private TextBox txtPassword;
         private Label label3;
-        private Button button2;
+        private Button btnSignUp;
         private Label label4;
+        private PictureBox pictureBox1;
     }
 }
