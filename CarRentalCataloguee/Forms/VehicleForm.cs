4f2b2d13a5e47ad06b167eb053e9f825bdd705d8
Gigson Aquino <gigsonaquino4@gmail.com>
2025-12-15 10:26:49 +0800

    Finally Done OOP Project School Year 2025 - 2026

--- CarRentalCataloguee/Forms/VehicleForm.cs
+++ CarRentalCataloguee/Forms/VehicleForm.cs
@@ -224,6 +224,7 @@ namespace CarRentalCataloguee.Forms
             System.Diagnostics.Debug.WriteLine("label1_Click fired at " + DateTime.Now.ToString("o"));
             //
             //
+            //
         }
     }
 }
